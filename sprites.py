be6cb3a2d14971d5bdd7df83d8e57f6ba5d03c1f
Vipershroom <undertalefan812@gmail.com>
2023-12-05 15:37:55 -0500

    Add rect to sprite class

--- sprites.py
+++ sprites.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import _Group
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x_pos, y_pos):
+    def __init__(self):
         super().__init__()
         self.image = pygame.image.load('assets/player')
+        self.rect = self.image.get_rect(center = (200,200))
