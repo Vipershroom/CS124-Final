ee006cff84c59c614660251715bf60c385ead4c4
Vipershroom <undertalefan812@gmail.com>
2023-12-05 15:38:05 -0500

    Add time and score to scene

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from constants import *
 pygame.init()
 
 screen = pygame.display.set_mode(window)
-
+score = 0
+clock = pygame.time.Clock()
 while True:
     for events in pygame.event.get():
         if events.type == pygame.QUIT:
             exit()
     pygame.display.update()
+    clock.tick(60)
