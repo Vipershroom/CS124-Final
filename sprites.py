156e2155d1bd9c6dfa7dbdd1c8b78053221c2551
Vipershroom <undertalefan812@gmail.com>
2023-12-08 03:11:07 -0500

    Add main menu

--- sprites.py
+++ sprites.py
@@ -1,8 +1,26 @@
 import pygame
-from pygame.sprite import _Group
+# from pygame.sprite import _Group
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('assets/player')
         self.rect = self.image.get_rect(center = (200,200))
+
+class Button(pygame.sprite.Sprite):
+    def __init__(self, text, pos) -> None:
+        super().__init__()
+        self.color = "#454647"
+        self.text = text
+        self.font = pygame.font.SysFont('MS Mincho', 50) 
+        self.image = self.font.render(text, False, self.color)
+        self.rect = self.image.get_rect(midbottom = pos)
+        self.active = False
+    def redraw(self):
+        if self.active:
+            self.color = "#f5f7fa"
+        else:
+            self.color = "#454647"
+        self.image = self.font.render(self.text, False, self.color)
+    def Broadcast(self):
+        return self.text
