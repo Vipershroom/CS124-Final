f5065deeacc00ae09ef7cef0377c93bc5c186350
Vipershroom <undertalefan812@gmail.com>
2023-12-08 14:02:57 -0500

    Add smooth movement to player

--- level1.py
+++ level1.py
@@ -9,11 +9,34 @@ def level1(screen: pygame.Surface):
 
     player = pygame.sprite.GroupSingle()
     player.add(Player())
-    while True:
-        for events in pygame.event.get():
-            if events.type == pygame.QUIT:
-                exit()
 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.sprite.left_pressed = True
+                if event.key == pygame.K_RIGHT:
+                    player.sprite.right_pressed = True
+                if event.key == pygame.K_UP:
+                    player.sprite.up_pressed = True
+                if event.key == pygame.K_DOWN:
+                    player.sprite.down_pressed = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    player.sprite.left_pressed = False
+                if event.key == pygame.K_RIGHT:
+                    player.sprite.right_pressed = False
+                if event.key == pygame.K_UP:
+                    player.sprite.up_pressed = False
+                if event.key == pygame.K_DOWN:
+                    player.sprite.down_pressed = False
+
+        
+        
         screen.blit(bg, (0,0))
+        player.draw(screen)
+        player.sprite.move()
         pygame.display.update()
         clock.tick(60)
--- sprites.py
+++ sprites.py
@@ -2,13 +2,34 @@ import pygame
 # from pygame.sprite import _Group
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, screen=None):
+    def __init__(self):
         super().__init__()
-        self.image = pygame.draw.circle(screen,"Red", (200,200), 10.)
-        self.rect = self.image.get_rect(center = (200,200))
+        self.image = pygame.transform.scale(pygame.image.load('assets/player/circle.png'), (100,100)).convert_alpha()
+        self.rect = self.image.get_rect(center = (200,600))
+        self.velX = 0
+        self.velY = 0
+        self.left_pressed = False
+        self.right_pressed = False
+        self.up_pressed = False
+        self.down_pressed = False
+        self.speed = 4
+        self.x = 100
+        self.y = 100
     
     def move(self):
-        pass
+        self.velX = 0
+        self.velY = 0
+        if self.left_pressed and not self.right_pressed:
+            self.velX = -self.speed
+        if self.right_pressed and not self.left_pressed:
+            self.velX = self.speed
+        if self.up_pressed and not self.down_pressed:
+            self.velY = -self.speed
+        if self.down_pressed and not self.up_pressed:
+            self.velY = self.speed
+        
+        self.rect.x += self.velX
+        self.rect.y += self.velY
 
 
 
