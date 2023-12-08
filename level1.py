bbe6ca4a1e0c29216547a6a93fc3ed0379d01fa6
Vipershroom <undertalefan812@gmail.com>
2023-12-08 13:10:12 -0500

    Add player sprite

--- level1.py
+++ level1.py
@@ -1,12 +1,14 @@
 import pygame
 from variables import window
-
+from sprites import Player
 def level1(screen: pygame.Surface):
     
     clock = pygame.time.Clock()
 
     bg = pygame.transform.scale(pygame.image.load('assets/background/sky.png'), window).convert()
 
+    player = pygame.sprite.GroupSingle()
+    player.add(Player())
     while True:
         for events in pygame.event.get():
             if events.type == pygame.QUIT:
